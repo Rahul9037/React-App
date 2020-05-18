81e48b6b31e594852715c7e60c0a3f3d1720ee60
Vipin GS <vipings@aol.in>
2020-05-18 16:53:10 +0530

    Updates

---
+++ precache-manifest.459f4038374a0ff874ee21434896c96b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65690b27b6c7c1f4feeb2dfa52e628b",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "9e0ca0d7f6ffeace1a8c",
+    "url": "/React-App/static/css/main.97f234ae.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/React-App/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0ca0d7f6ffeace1a8c",
+    "url": "/React-App/static/js/main.de4d2734.chunk.js"
+  },
+  {
+    "revision": "612d6b817965d6a66f7f",
+    "url": "/React-App/static/js/runtime-main.d04f09ee.js"
+  }
+]);
